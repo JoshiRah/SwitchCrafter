c995a7de44a51402529b4420b6a666a8a2b137e0
J.Rahmlow <j.rahmlow@rg-diakonie.de>
2025-10-22 21:40:44 +0200

    Initial commit

---
+++ modules.py
@@ -0,0 +1,157 @@
+# error messages
+
+errorCode060 = 'No available parameter for this function'
+errorCode070 = 'Missing or wrong parameters'
+errorCode080 = 'Look at the mirror and you will see the problem'
+errorCode999 = 'Fatal error. Just burn your pc'
+
+
+# the one and only class
+
+class Switch():
+    def __init__(self, model=None, ports=None, hostname=None, username=None, password=None, vidData=None, vidVoice=None, vidManagement=None, vidIot=None, vidGuests=None, nameData=None, nameVoice=None, nameManagement=None, nameIot=None, nameGuests=None, managementVlan=None, ipAddr=None, netMask=None, defGW=None, dnsServer=None, domainName=None, community=None, contact=None, location=None, remoteServer=None, ntpServer=None):
+        self.model = model
+        self.ports = ports
+        self.hostname = hostname
+        self.uname = username
+        self.passwd = password
+        self.vidDT = vidData
+        self.vidVC = vidVoice
+        self.vidMG = vidManagement
+        self.vidIO = vidIot
+        self.vidGU = vidGuests
+        self.nameDT = nameData
+        self.nameVC = nameVoice
+        self.nameMG = nameManagement
+        self.nameIO = nameIot
+        self.nameGU = nameGuests
+        self.vlan = managementVlan
+        self.ip = ipAddr
+        self.mask = netMask
+        self.gw = defGW
+        self.dns = dnsServer
+        self.domain = domainName
+        self.comm = community
+        self.cont = contact
+        self.loc = location
+        self.rs = remoteServer
+        self.ntp = ntpServer
+
+    def CheckDataIntegrity(self):
+        while True:
+            try:
+                return all(value not in (None, '', []) for value in self.__dict__.values())
+                break
+            except:
+                print(errorCode999)
+
+    def BuildHostname(self):
+         validHostname = f'hostname {self.hostname}'
+         return validHostname
+
+    def BuildLocalAdmin(self):
+        if self.model == 'hpe':
+            validUser = f'username {self.uname} password plaintext {self.passwd}\n'
+            return validUser
+        elif self.model == 'dell':
+            validUser = f'user {self.uname} password {self.passwd} privilege 15\n'
+            return validUser
+        else:
+            return errorCode060
+
+    def CreateVLAN(self):
+        validVLAN = f'vlan {self.vidDT}\nname {self.nameDT}\nvlan {self.vidVC}\nname {self.nameVC}\nvlan {self.vidMG}\nname {self.nameMG}\nvlan {self.vidIO}\nname {self.nameIO}\nvlan {self.vidGU}\nname {self.nameGU}\nexit\n'
+        return validVLAN
+
+    def CreateInterface(self):
+        if self.model == 'hpe':
+            validInterface = f'interface vlan {self.vlan}\nip address {self.ip} {self.mask}\nexit\nip route 0.0.0.0/0 {self.gw}\n'
+            return validInterface
+        if self.model == 'dell':
+            validInterface = f'interface vlan {self.vlan}\nip address {self.ip} {self.mask}\nexit\nip default-gateway {self.gw}\n'
+            return validInterface
+        else:
+            return errorCode060
+
+    def CreateDNS(self):
+        if self.model == 'hpe':
+            validDNS = f'ip dns server-address {self.dns}\nip dns domain-name {self.domain}\n'
+            return validDNS
+        elif self.model == 'dell':
+            validDNS = f'ip name-server {self.dns}\nip domain-name {self.domain}\n'
+            return validDNS
+        else:
+            return errorCode060
+
+    def CreateSyslog(self):
+        if self.model == 'hpe':
+            validLogging = f'logging {self.rs} severity info\n'
+            return validLogging
+        elif self.model == 'dell':
+            validLogging = f'logging {self.rs}\nlevel informational\nexit\n'
+            return validLogging
+        else:
+            return errorCode060
+
+    def CreateSNMP(self):
+        if self.model == 'hpe':
+            validSNMP = f'snmp-server vrf default\nsnmp-server community {self.comm}\nsnmp-server system-contact {self.cont}\nsnmp-server system-location {self.loc}\n'
+            return validSNMP
+        elif self.model == 'dell':
+            validSNMP = f'snmp-server community {self.comm}\nsnmp-server contact {self.cont}\nsnmp-server {self.loc}\n'
+            return validSNMP
+        else:
+            return errorCode060
+
+    def CreateNTP(self):
+        if self.model == 'hpe':
+            validNTP = f'ntp server {self.ntp}\nclock timezone europe/berlin\n'
+            return validNTP
+        elif self.model == 'dell':
+            validNTP = f'sntp server {self.ntp}\nclock timezone +2\n'
+            return validNTP
+        else:
+            return errorCode060
+
+    def CreateUplinks(self):
+        if self.model == 'hpe' and self.ports == 12:
+            validUplinks = f'interface 1/1/13-1/1/16\nvlan trunk allowed all\nvlan trunk native 1\ndescription Network\nspanning-tree port-type admin-network\nno shutdown\nexit\n' # Set interfaces 13 - 16 into vlan trunk mode with native trunk on vlan 1, set the description to Network and port-type into network
+            return validUplinks
+        elif self.model == 'hpe' and self.ports == 24:
+            validUplinks = f'interface 1/1/23-1/1/28\nvlan trunk allowed all\nvlan trunk native 1\ndescription Network\nspanning-tree port-type admin-network\nno shutdown\nexit\n' # Set interfaces 23 - 28 into vlan trunk mode with native trunk on vlan 1, set the description to Network and port-type into network
+            return validUplinks
+        elif self.model == 'hpe' and self.ports == 48:
+            validUplinks = f'interface 1/1/47-1/1/52\nvlan trunk allowed all\nvlan trunk native 1\ndescription Network\nspanning-tree port-type admin-network\nno shutdown\nexit\n' # Set interfaces 47 - 52 into vlan trunk mode with native trunk on vlan 1, set the description to Network and port-type into network
+            return validUplinks
+        elif self.model == 'dell' and self.ports == 24:
+            validUplinks = (f'interface range gi1/0/23-24\nswitchport trunk allowed vlan all\nswitchport trunk native vlan 1\nswitchport mode trunk\ndescription Network\nno shutdown\nexit\n' # Set interfaces gi23 - gi24 into vlan trunk mode with native trunk on vlan 1, set the description to Network and spanning-tree into network
+                            f'interface range te1/0/1-4\nswitchport trunk allowed vlan all\nswitchport trunk native vlan 1\nswitchport mode trunk\ndescription Network\nno shutdown\nexit\n') # Set interfaces te1 - te4 into vlan trunk mode with native trunk on vlan 1, set the description to Network and spanning tree into network
+            return validUplinks
+        elif self.model == 'dell' and self.ports == 48:
+            validUplinks = (f'interface range gi1/0/47-48\nswitchport trunk allowed vlan all\nswitchport trunk native vlan 1\nswitchport mode trunk\ndescription Network\nno shutdown\nexit\n' # Set interfaces gi23 - gi24 into vlan trunk mode with native trunk on vlan 1, set the description to Network and spanning-tree into network
+                            f'interface range te1/0/1-4\nswitchport trunk allowed vlan all\nswitchport trunk native vlan 1\nswitchport mode trunk\ndescription Network\nno shutdown\nexit\n') # Set interfaces te1 - te4 into vlan trunk mode with native trunk on vlan 1, set the description to Network and spanning tree into network
+            return validUplinks
+        else:
+            return errorCode060
+
+    def CreateAccess(self):
+        if self.model == 'hpe' and self.ports == 12:
+            validUplinks = f'interface 1/1/1-1/1/12\nvlan trunk allowed all\nvlan trunk native {self.vidDT}\ndescription Edge\nspanning-tree port-type admin-edge\nno shutdown\nexit\n' # Set interfaces 13 - 16 into vlan trunk mode with native trunk on vlan 100, set the description to Edge and port-type into edge
+            return validUplinks
+        elif self.model == 'hpe' and self.ports == 24:
+            validUplinks = f'interface 1/1/1-1/1/22\nvlan trunk allowed all\nvlan trunk native {self.vidDT}\ndescription Edge\nspanning-tree port-type admin-edge\nno shutdown\nexit\n' # Set interfaces 23 - 28 into vlan trunk mode with native trunk on vlan 100, set the description to Edge and port-type into edge
+            return validUplinks
+        elif self.model == 'hpe' and self.ports == 48:
+            validUplinks = f'interface 1/1/1-1/1/46\nvlan trunk allowed all\nvlan trunk native {self.vidDT}\ndescription Edge\nspanning-tree port-type admin-edge\nno shutdown\nexit\n' # Set interfaces 47 - 52 into vlan trunk mode with native trunk on vlan 100, set the description to Edge and port-type into edge
+            return validUplinks
+        elif self.model == 'dell' and self.ports == 24:
+            validUplinks = (f'interface range gi1/0/1-22\nswitchport general pvid {self.vidDT}\nswitchport allowed vlan add {self.vidDT} untagged\nswitchport general allowed vlan add 100-999 tagged\n'
+                            f'switchport general allowed vlan remove 1\nswitchport mode general\ndescription Edge\nspanning-tree portfast bpdufilter default\nno shutdown\nexit\n') # Set interfaces gi1 - gi22 into vlan general mode with native trunk on vlan data, set the description to Network and spanning-tree into edge
+            return validUplinks
+        elif self.model == 'dell' and self.ports == 48:
+            validUplinks = (f'interface range gi1/0/1-46\nswitchport general pvid {self.vidDT}\nswitchport allowed vlan add {self.vidDT} untagged\nswitchport general allowed vlan add 100-999 tagged\n'
+                            f'switchport general allowed vlan remove 1\nswitchport mode general\ndescription Edge\nspanning-tree portfast bpdufilter default\nno shutdown\nexit\n') # Set interfaces gi1 - gi22 into vlan general mode with native trunk on vlan data, set the description to Network and spanning-tree into edge
+            return validUplinks
+        else:
+            return errorCode060
+
---
+++ program.py
@@ -0,0 +1,135 @@
+import modules
+import tkinter as tk
+from tkinter import messagebox, Label, Frame
+from tkinter import ttk
+
+import os
+
+models = ['HP Enterprise Aruba 6000', 'Dell EMC N1500']
+version = ['12 Port', '24 Port', '48 Port']
+ifVids = ['1', '300', '400']
+
+newSwitch = modules.Switch('dell', 48, 'SW01', 'admin','geheim',100,200,300,400,900,'NET-DATA','NET-VOICE','NET-MGMT','NET-IOT','NET-GUESTS','300','1.1.1.1','255.255.255.0', '1.1.1.2', '1.2.3.4', 'rg-diakonie.de', 'public', 'noc@rg-diakonie.de', 'hier', 'syslog.rg-diakonie.de', 'ntp.rg-diakonie.de')
+
+# main window
+root = tk.Tk()
+root.title('Switch configuration generator | J. Rahmlow (c) 2025')
+root.geometry('800x600')
+
+# placeholder
+tk.Label(root, text='').grid(row=3,column=0)
+tk.Label(root, text='').grid(row=5,column=0)
+tk.Label(root, text='').grid(row=7,column=0)
+tk.Label(root, text='').grid(row=13,column=0)
+tk.Label(root, text='').grid(row=15, column=0)
+tk.Label(root, text='').grid(row=17, column=0)
+#tk.Label(root, text='').grid(row=20, column=0)
+#tk.Label(root, text='').grid(row=23, column=0)
+#tk.Label(root, text='').grid(row=28,column=0)
+#tk.Label(root, text='').grid(row=0,column=0)
+
+
+# model
+tk.Label(root, text='Model:').grid(row=1, column=0)
+givenModel = ttk.Combobox(root, values=models)
+givenModel.set('Select a model')
+givenModel.grid(row=1, column=1)
+
+# count of ports
+tk.Label(root, text='Version:').grid(row=2,column=0)
+givenVersion = ttk.Combobox(root, values=version)
+givenVersion.set('Select a version')
+givenVersion.grid(row=2, column=1)
+
+# hostname
+tk.Label(root, text='Hostname:').grid(row=4, column=0)
+givenHostname = tk.Entry(root)
+givenHostname.grid(row=4, column=1)
+
+# username
+tk.Label(root, text='Username: ').grid(row=6,column=0)
+givenUsername = tk.Entry(root)
+givenUsername.grid(row=6, column=1)
+
+# password
+tk.Label(root, text='Password: ').grid(row=6, column=3)
+givenPassword = tk.Entry(root)
+givenPassword.grid(row=6, column=4)
+
+# data vlan
+tk.Label(root, text='DATA-Vid:').grid(row=8,column=0)
+givenVidData = tk.Entry(root)
+givenVidData.grid(row=8, column=1)
+
+tk.Label(root, text='DATA-Name:').grid(row=8,column=3)
+givenNameData = tk.Entry(root)
+givenNameData.grid(row=8, column=4)
+
+# voice vlan
+tk.Label(root, text='VOICE-Vid:').grid(row=9,column=0)
+givenVidVoice = tk.Entry(root)
+givenVidVoice.grid(row=9, column=1)
+
+tk.Label(root, text='VOICE-Name:').grid(row=9,column=3)
+givenNameVoice = tk.Entry(root)
+givenNameVoice.grid(row=9, column=4)
+
+# mgmt vlan
+tk.Label(root, text='MGMT-Vid:').grid(row=10,column=0)
+givenVidMgmt = tk.Entry(root)
+givenVidMgmt.grid(row=10, column=1)
+
+tk.Label(root, text='MGMT-Name:').grid(row=10,column=3)
+givenNameMgmt = tk.Entry(root)
+givenNameMgmt.grid(row=10, column=4)
+
+# iot vlan
+tk.Label(root, text='IOT-Vid:').grid(row=11,column=0)
+givenVidIot = tk.Entry(root)
+givenVidIot.grid(row=11, column=1)
+
+tk.Label(root, text='IOT-Name:').grid(row=11,column=3)
+givenNameIot = tk.Entry(root)
+givenNameIot.grid(row=11, column=4)
+
+# guests vlan
+tk.Label(root, text='GUESTS-Vid:').grid(row=12,column=0)
+givenVidGuests = tk.Entry(root)
+givenVidGuests.grid(row=12, column=1)
+
+tk.Label(root, text='GUESTS-Name:').grid(row=12,column=3)
+givenNameGuests = tk.Entry(root)
+givenNameGuests.grid(row=12, column=4)
+
+# management interface settings
+tk.Label(root, text='Interface-Vid:').grid(row=14,column=0)
+givenIfVid = tk.Entry(root)
+givenIfVid.grid(row=14, column=1)
+
+tk.Label(root, text='Interface-IP:').grid(row=16,column=0)
+givenIfIp = tk.Entry(root)
+givenIfIp.grid(row=16, column=1)
+
+tk.Label(root, text='Netmask:').grid(row=16, column=3)
+givenNetMask = tk.Entry(root)
+givenNetMask.grid(row=16, column=4)
+
+tk.Label(root, text='Default Gateway:').grid(row=16, column=6)
+givenGateway = tk.Entry(root)
+givenGateway.grid(row=16, column=7)
+
+tk.Label(root, text='DNS-Server:').grid(row=18, column=0)
+givenDNS = tk.Entry(root)
+givenDNS.grid(row=18, column=1)
+
+tk.Label(root, text='Domain name:').grid(row=18, column=3)
+givenDomain = tk.Entry(root)
+givenDomain.grid(row=18, column=4)
+
+
+# place button
+#button = tk.Button(root, text="Save values", command=None)
+#button.grid(row=5, column=1)
+
+# start main loop
+root.mainloop()
